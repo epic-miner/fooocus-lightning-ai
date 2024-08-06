aa9164a3a7d88d98f21e9bd6fe480a6f5bb17d60
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-08-06 05:51:56 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -92,7 +92,7 @@ fi
 conda activate fooocus
 cd ..
 if [ $# -eq 0 ]; then
-    python Fooocus/entry_with_update.py 
+    python Fooocus/entry_with_update.py --always-high-vram
 elif [ $1 = "reset" ]; then
     python Fooocus/entry_with_update.py --always-high-vram --reset
 fi
